ff93b56144968e6def8f8714b5c7d43dadc35bf6
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2022-07-20 15:40:00 +0530

    package

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import Navbar from "./Components/Navbar";
 import './App.css'
 import Intro from "./Components/Intro/Intro";
 import Services from "./Components/Services/Services";
+import Experience from "./Components/Experience/Experience";
+import Works from "./Components/Works/Works";
+import Testimonial from "./Components/Testimonials/Testimonials";
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <Intro />
     <Services />
+    <Experience />
+    <Works />
+    <Testimonial />
     </div>
   );
 }
